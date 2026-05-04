2fab896cfd9ad56103ad1175004cb6ff99e53c32
Johan Nguyen <johannsevenn@gmail.com>
2026-05-04 17:58:51 -0400

    scaffold data modules with stub crud

---
+++ data/events.js
@@ -0,0 +1,25 @@
+const create = async (eventObj) => {
+  throw new Error("Not implemented");
+};
+
+const getAll = async () => {
+  throw new Error("Not implemented");
+};
+
+const getById = async (id) => {
+  throw new Error("Not implemented");
+};
+
+const search = async (filters) => {
+  throw new Error("Not implemented");
+};
+
+const update = async (id, updates) => {
+  throw new Error("Not implemented");
+};
+
+const remove = async (id) => {
+  throw new Error("Not implemented");
+};
+
+module.exports = { create, getAll, getById, search, update, remove };
---
+++ data/users.js
@@ -0,0 +1,29 @@
+const create = async (firstName, lastName, email, handle, password, borough) => {
+  throw new Error("Not implemented");
+};
+
+const getAll = async () => {
+  throw new Error("Not implemented");
+};
+
+const getById = async (id) => {
+  throw new Error("Not implemented");
+};
+
+const getByEmail = async (email) => {
+  throw new Error("Not implemented");
+};
+
+const getByHandle = async (handle) => {
+  throw new Error("Not implemented");
+};
+
+const update = async (id, updates) => {
+  throw new Error("Not implemented");
+};
+
+const remove = async (id) => {
+  throw new Error("Not implemented");
+};
+
+module.exports = { create, getAll, getById, getByEmail, getByHandle, update, remove };
